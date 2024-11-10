10001e129d8476157ad53b7521cf8ce3646d4680
fe4rlisa <eleontiev1@gmail.com>
2024-11-10 17:32:08 -0500

    Removed scale var and fixed centering logic

--- HexMapEdit.py
+++ HexMapEdit.py
@@ -8,17 +8,19 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+
+BASE_SCREEN_WIDTH = 800
+BASE_SCREEN_HEIGHT = 600
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 800,600#screen.get_size()
 SCREEN_CENTER = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
 
-radius = 20
+radius = 34
 hex_width = np.sqrt(3) * radius
-hex_height = 2 * radius
+hex_height = 3 / 2 * radius
 grid_width = 10
 grid_height = 10
-scale = 40
 grid = []
 
 
@@ -30,8 +32,8 @@ def create_grid(grid_width, grid_height):
     return grid
 
 def draw_grid(grid):
-    grid_pixel_width = (grid_width - 1) * scale
-    grid_pixel_height = (grid_height - 1) * scale
+    grid_pixel_width = (grid_width) *  (hex_width) - (hex_width / 2)
+    grid_pixel_height = (grid_height) * (hex_height) - (hex_width)
 
     start_x = SCREEN_CENTER[0] - grid_pixel_width / 2
     start_y = SCREEN_CENTER[1] - grid_pixel_height / 2
@@ -39,18 +41,31 @@ def draw_grid(grid):
     for y in range(grid_height):
         for x in range(grid_width):
             grid_x_location = start_x + (hex_width) * x
-            grid_y_location = start_y + (hex_height * .77) * y
+            grid_y_location = start_y + (hex_height) * y
             if (y%2 == 0):
                 grid_x_location += hex_width / 2
 
             angles = np.linspace(np.pi / 6, 11 * np.pi / 6, 6)
             hexagon_vertices = [(grid_x_location + radius * np.cos(angle), grid_y_location + radius * np.sin(angle)) for angle in angles]
             
-            hexagon = pygame.draw.polygon(screen, BLACK, hexagon_vertices, width=2)
-            #pygame.draw.circle(screen, BLUE, (grid_x_location, grid_y_location), radius, width=2)
+            # <For debug> changes color of odd x heagons to red
+            if (x%2 == 0):
+                pygame.draw.polygon(screen, BLACK, hexagon_vertices, width=4)
+            else:
+                hexagon_inner = pygame.draw.polygon(screen, (200,200,200), hexagon_vertices)
+                hexagon_outer = pygame.draw.polygon(screen, BLACK, hexagon_vertices, width=4)
+
+# Need to translate center cords to real cords???
+def cord_to_real(grid):
+    start_x = SCREEN_CENTER[0] - grid_pixel_width / 2
+    start_y = SCREEN_CENTER[1] - grid_pixel_height / 2
+    grid_x_location = start_x + (hex_width) * x
+    grid_y_location = start_y + (hex_height) * y
+    
 
 pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((BASE_SCREEN_WIDTH, BASE_SCREEN_HEIGHT))
+pygame.display.set_caption("HexMapEdit")
 clock = pygame.time.Clock()
 running = True
 
@@ -65,6 +80,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.VIDEORESIZE:
+            WIDTH, HEIGHT = event.w, event.h
+            screen = pygame.display.set_mode((WIDTH,HEIGHT), pygame.RESIZABLE)
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
